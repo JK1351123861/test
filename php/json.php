d6a07bb57965b5df0a39eb455adfa2cabbbd0d7b
JK1351123861 <1351123861@qq.com>
2017-08-23 17:26:28 +0800

    add php

---
+++ php/json.php
@@ -0,0 +1,26 @@
+<?php
+	echo '{
+		"form":
+		[
+			{
+				"href":"index/task.html",
+				"type":"请假"
+			},
+
+			{
+				"href":"index/cost.html",
+				"type":"费用"
+			},
+
+			{
+				"href":"index/go_out.html",
+				"type":"出差"
+			},
+
+			{
+				"href":"index/shopping.html",
+				"type":"采购"
+			}
+		]
+	}';
+?>
